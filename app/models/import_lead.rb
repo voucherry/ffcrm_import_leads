16447fbde48dd53c0506dc1348361a52a47246ea
Graham Sherry <gjsherry@gmail.com>
2014-01-20 16:57:58 +0000

    refactor import_leads, create/ accounts (no duplicates)

--- app/models/import_lead.rb
+++ app/models/import_lead.rb
@@ -13,20 +13,62 @@ class ImportLead
     import
   end
 
-  # Sample Format
-  # "campaign_id","source","tag","created_at","first_name","last_name","email","phone","company","title","status","background_info","comments","street1","street2","city","state","zipcode","country"
+  # Sample Format (* = required)
+  # "campaign_id","source","tag","created_at","first_name"*,"last_name"*,"email","phone","company","title","status","background_info","comments","street1","street2","city","state","zipcode","country"
   def import
     CSV.foreach(@file.path, :converters => :all, :return_headers => false, :headers => :first_row) do |row|
       campaign_id, source, tag, created_at, first_name, last_name,
       email, phone, company, title, status, background_info, comments,
       street1, street2, city, state, zip, country = *row.to_hash.values
 
-      #TODO: check for duplicates based on email (include contacts)
+      #TODO: implement smarter_csv and/or resque
+      # https://github.com/tilo/smarter_csv
+      # https://github.com/resque/resque
 
-      lead = Lead.new(:campaign_id => campaign_id.to_i, :source => source,
-        :first_name => first_name, :last_name => last_name, :email => email, :phone => phone,
-        :company => company, :title => title, :status => status,
-        :background_info => background_info, :user_id => @assigned.id, :created_at => created_at.to_time)
+      #TODO: Handle duplicates on first_name, last_name, email (should also stop dup contacts)
+
+      # entries.each do |entry|
+      #   unless exists? :email => lead.email
+      #     create!(
+      #       :first_name        => lead.first_name,
+      #       :last_name        => lead.last_name,
+      #     )
+      #   end
+      # end
+
+      # Lead.where('lower(email) = ?', "dillan@example.com").first
+
+      #TODO: rails 4 Lead.find_or_initialize_by(email: email) without tap
+      # Don't Allow Duplicates
+      # lead = Lead.find_or_initialize_by_email(email).tap do |lead|
+      #         lead.user_id = @assigned.id,
+      #         lead.campaign_id = campaign_id.to_i,
+      #         lead.source = source,
+      #         lead.first_name = first_name,
+      #         lead.last_name = last_name,
+      #         lead.email = email,
+      #         lead.phone = phone,
+      #         lead.company = company,
+      #         lead.title = title, lead.status = status,
+      #         lead.background_info = background_info,
+      #         lead.created_at = created_at.to_time
+      #         end
+      # lead.save!
+
+      # Allow Duplicates
+      lead = Lead.new(
+        :user_id => @assigned.id,
+        :campaign_id => campaign_id.to_i,
+        :source => source,
+        :first_name => first_name,
+        :last_name => last_name,
+        :email => email,
+        :phone => phone,
+        :company => company,
+        :title => title, :status => status,
+        :background_info => background_info,
+        :created_at => created_at.to_time
+        )
 
       lead.first_name = "INCOMPLETE" if lead.first_name.blank?
       lead.last_name = "INCOMPLETE" if lead.last_name.blank?
@@ -34,40 +76,46 @@ class ImportLead
       # lead.access? = "Private | Public"
       lead.access = Setting.default_access
 
-      lead.business_address = Address.new(:street1 => street1, :street2 => street2, :city => city, :state => state, :zipcode => zip, :country => country, :address_type => "Business")
-
       lead.assignee = @assigned if @assigned.present?
+
+      #TODO: Better validation on address fields.
+      if zip
+        lead.business_address = Address.new(:street1 => street1, :street2 => street2, :city => city, :state => state, :zipcode => zip, :country => country, :address_type => "Business")
+      end
+
       lead.save!
 
       if @promote_leads
 
-        # Rails.logger.info "XXXXXXXX PROMOTE LEAD!"
-        # @account, @opportunity, @contact = lead.promote(:account => lead.company)
-
-        account = Account.find_or_initialize_by_name(lead.company).tap do |account|
-          account.assignee = @assigned if @assigned.present?
-          account.access = Setting.default_access
+        if lead.company
+          account_name = lead.company
+        else
+          account_name = lead.first_name + ' ' + lead.last_name + ' (Individual)'
         end
+
+        #TODO: rails 4 Account.find_or_initialize_by(name: account_name)
+        account = Account.find_or_initialize_by_name(account_name).tap do |account|
+                    account.user_id = @assigned.id
+                    account.assignee = @assigned if @assigned.present?
+                    account.access = lead.access
+                    account.category = 'customer'
+                  end
         account.save!
 
-        @account, @opportunity, @contact = lead.promote(:account => account["id"])
+        lead_account = { account: { id: account.id }, access: lead.access }
 
-        Rails.logger.info "XXXXXXXX ERROR! Account:#{@account.errors} Contact: #{@contact.errors}"
+        @account, @opportunity, @contact = lead.promote(lead_account)
 
-        # Contact.find_or_initialize_by_email(lead.email).tap do |contact|
-        #   contact.access = Setting.default_access
-        #   contact.assignee = @assigned if @assigned.present?
-        # end.save!
-
+        #TODO: Consider handling duplicates on first_name, last_name, email
         contact = Contact.find(@contact)
         contact.tag_list.add(tag)
         contact.add_comment_by_user(comments, @assigned)
         contact.save!
 
-        Rails.logger.info "XXXXXXXX CONTACT CREATED!"
-
         lead.convert
         lead.save!
+
+        # Rails.logger.info "XXXXXXXX CONTACT CREATED! #{contact.inspect}"
 
       else
         lead.tag_list.add(tag)
